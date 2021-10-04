2061b33e93e64e56a5dd24b9112ba5f007f2681c
SEIFU MEKONNEN TOLOSA <tms.18@yahoo.com>
2021-10-04 14:20:43 +0100

    add next-auth react hook

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,14 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
+import Providers from "next-auth/providers";
 const prisma = new PrismaClient();
-
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [
-    GoogleProvider({
+    Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from "next/head";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
+  const [session] = useSession();
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <>
-        <h1>Card Hub App</h1>
+        {!session && (
+          <>
+            Not signed in <br /> <button onClick={() => signIn()}>Sign in</button>
+          </>
+        )}
+
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
       </>
     </div>
   );
